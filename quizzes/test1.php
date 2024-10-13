030505820c0126240194a6b8ea5d6c045e6e7588
NwoguGlory1 <glorynwogu18@gmail.com>
2024-10-13 10:42:45 +0100

    Quiz2

---
+++ quizzes/test1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+//Variable name in PHP
+$name = "Joe";
+?>  
+</body>
+</html>
---
+++ quizzes/test2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$greeting = 'Hello World';
+// Render the $greeting string to the page.
+// To output a string to the page, which PHP keyword might we use?
+ print "$greeting";
+ 
+?>
+</body>
+</html>
