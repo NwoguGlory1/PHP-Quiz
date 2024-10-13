ca8b6ea6dbd8bea3c7a2a538178c38e244b7555a
NwoguGlory1 <glorynwogu18@gmail.com>
2024-10-13 11:59:44 +0100

    Quiz6

---
+++ quizzes/test6.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$confirmed = false;
+
+if (!$confirmed) {
+echo 'This appointment has not yet been confirmed.';
+}
+
+// This code includes a bug. Update it to check if the appointment has NOT been
+// confirmed.
+</body>
+</html>
