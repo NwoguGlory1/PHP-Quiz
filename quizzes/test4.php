51d21a11fce75e1cf5585cb03e1cfe3aff3a5a2b
NwoguGlory1 <glorynwogu18@gmail.com>
2024-10-13 10:54:07 +0100

    Quiz4

---
+++ quizzes/test4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Joe is 25, and Jane is 30.
+
+$names = [
+	'Joe' => 25,
+	'Jane' => 30
+];
+
+// Arrays can optionally include keys. Update the list of names below to include
+// the person's respective age.
+?>
+</body>
+</html>
