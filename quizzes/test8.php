7191bbf107fc913457a5236a99ba131e7b002485
NwoguGlory1 <glorynwogu18@gmail.com>
2024-10-13 12:15:38 +0100

    Quiz8

---
+++ quizzes/test8.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+function greet($thing) {
+   return"Hello there, $thing.";
+}
+
+// If a function should return a value, which keyword should we use? Replace the
+// ... placeholder with the correct keyword.
+
+?>
+</body>
+</html>
