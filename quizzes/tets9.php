4652148d2e54957d825f463a8f0764491e47443c
NwoguGlory1 <glorynwogu18@gmail.com>
2024-10-13 12:17:42 +0100

    Quiz9

---
+++ quizzes/tets9.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+function isManager($person) {
+	return $person['role'] === 'Manager';
+}
+
+// What is the correct way to determine equality? Update the ... placeholder with
+// the appropriate comparison.
+?>
+</body>
+</html>
