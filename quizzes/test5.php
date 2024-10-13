c819ba85b65b941af69d87728b7c39b0d4701ffc
NwoguGlory1 <glorynwogu18@gmail.com>
2024-10-13 10:57:36 +0100

    Quiz5

--- quizzes/test5.php
+++ quizzes/test5.php
@@ -1,9 +1,19 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <body>
 
 <?php
 
+$post = [
+    'title' => 'My Blog Post',
+    'author' => 'LaryRobot',
+    'slug' => "my-blog-post"
+];
+
+// Consider this $post array. It currently includes a title and an author. Update
+// it to also include a slug key that is equal to "my-blog-post".
+
+
 ?>
 </body>
-</html> -->
+</html>
