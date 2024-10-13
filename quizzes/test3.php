93ffd435ec9d93d710963fb9c6029854d26e86d8
NwoguGlory1 <glorynwogu18@gmail.com>
2024-10-13 10:37:56 +0100

    Quiz3

---
+++ quizzes/test3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$popularNames = ['Oliver' ,'Violet' ,'Noah' , 'Aurora'];
+
+// We need to create a list of popular baby names, but there's currently a syntax
+// error. Fix the bug.
+
+?>
+</body>
+</html>
