f15cbe624f5578269c739ecc0de4c3a5860b54b2
NwoguGlory1 <glorynwogu18@gmail.com>
2024-10-28 14:46:35 +0100

    Looping over the fetched array

--- Notes_App/index.php
+++ Notes_App/index.php
@@ -3,14 +3,26 @@
 require "functions.php";
 // require 'router.php';
 
-// connect to MySQL databse;
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+class Database {
+    public function query()
+    {
+        // connect to MySQL databse;
+    $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
 
-$pdo = new PDO($dsn);
+    $pdo = new PDO($dsn);
 
-$statement = $pdo->prepare("select * from posts");
-$statement ->execute();
+    $statement = $pdo->prepare("select * from posts");
+    $statement ->execute();
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
 
-dd($posts);
+    }
+}
+
+$db = new Database();
+// instance of the Database class
+$posts = $db->query();
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] .  "</li>" ;
+}
