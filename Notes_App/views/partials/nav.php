5cc04a94dd8bd355ee85e4054b9d7428feb52c9e
NwoguGlory1 <glorynwogu18@gmail.com>
2024-10-28 12:27:08 +0100

    Notes is rendering 404 here

--- Notes_App/views/partials/nav.php
+++ Notes_App/views/partials/nav.php
@@ -8,10 +8,11 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-              <a href="/about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-              <a href="/contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
-            </div>
+              <a href="/" class="<?=urlIs('/') ? 'bg-gray-900 text-white': 'text-gray-300' ?> hover:bg-gray-700  px-3 py-2 rounded-md text-sm font-medium" >Home</a>
+              <a href="/about" class="<?=urlIs('/about.php') ? 'bg-gray-900 text-white': 'text-gray-300' ?> hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium" >About</a>
+              <a href="/note" class="<?=urlIs('/note.php') ? 'bg-gray-900 text-white': 'text-gray-300' ?> hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium" >Notes</a>
+              <a href="/contact" class="<?=urlIs('/contact.php') ? 'bg-gray-900 text-white': 'text-gray-300' ?> hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium" >Contact</a>
+              </div>
           </div>
         </div>
         <div class="hidden md:block">
