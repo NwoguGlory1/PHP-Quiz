06a46f69405a8e68c7e935e5550c68a5e485e0eb
NwoguGlory1 <glorynwogu18@gmail.com>
2024-10-28 13:38:37 +0100

    Connection with MySQL was successful to retrieve posts table

--- Notes_App/index.php
+++ Notes_App/index.php
@@ -1,4 +1,16 @@
 <?php
 
 require "functions.php";
-require 'router.php';
+// require 'router.php';
+
+// connect to MySQL databse;
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement ->execute();
+
+$posts = $statement->fetchAll();
+
+dd($posts);
