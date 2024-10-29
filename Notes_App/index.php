faab610bc6dee41dd25b3a41e387368d5e55a1d5
NwoguGlory1 <glorynwogu18@gmail.com>
2024-10-29 14:27:31 +0100

    Notes on nav bar is displaying 404 on browser here

--- Notes_App/index.php
+++ Notes_App/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require "functions.php";
-// require 'router.php';
+require 'router.php';
 require 'Database.php';
 
 $config = require('config.php');
@@ -10,8 +10,8 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $id = $_GET['id'];
-$query = "select * from posts where id = {$id}";
+$query = "select * from posts where id = ?";
 
-$posts = $db->query($query)->fetch();
+$posts = $db->query($query, [$id])->fetch();
 
 dd($posts);
