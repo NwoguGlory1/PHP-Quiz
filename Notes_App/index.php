221fc50d3b4e86ef45bc56dc37dd8f06ad730711
NwoguGlory1 <glorynwogu18@gmail.com>
2024-10-29 14:02:08 +0100

    Code Against Sequel Injection

--- Notes_App/index.php
+++ Notes_App/index.php
@@ -2,27 +2,16 @@
 
 require "functions.php";
 // require 'router.php';
+require 'Database.php';
 
-class Database {
-    public function query()
-    {
-        // connect to MySQL databse;
-    $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+$config = require('config.php');
 
-    $pdo = new PDO($dsn);
-
-    $statement = $pdo->prepare("select * from posts");
-    $statement ->execute();
-
-    return $statement->fetchAll(PDO::FETCH_ASSOC);
-
-    }
-}
-
-$db = new Database();
 // instance of the Database class
-$posts = $db->query();
+$db = new Database($config['database']);
 
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] .  "</li>" ;
-}
+$id = $_GET['id'];
+$query = "select * from posts where id = ?";
+
+$posts = $db->query($query, [$id])->fetch();
+
+dd($posts);
