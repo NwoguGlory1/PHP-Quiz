c8e849148c81e9ccdacabfc28f90f16900469bb5
NwoguGlory1 <glorynwogu18@gmail.com>
2024-11-09 23:50:06 +0100

    Create note view is rendering and is submitted to current page

--- Notes_App/views/notes.view.php
+++ Notes_App/views/notes.view.php
@@ -5,7 +5,6 @@
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <ul>
-         <!-- Your content -->
        <?php foreach ($notes as $note): ?>
         <li>
           <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
