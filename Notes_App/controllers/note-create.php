9a8b3d1b4c6500405c2599c7fa71fa9d20c52b02
NwoguGlory1 <glorynwogu18@gmail.com>
2024-11-10 17:11:09 +0100

    Created notes are now being retained on the input field where user types in to create note, even if the page is refreshed

--- Notes_App/controllers/note-create.php
+++ Notes_App/controllers/note-create.php
@@ -1,9 +1,28 @@
 <?php
+$config = require('config.php');
+
+// instance of the Database class
+$db = new Database($config['database']);
 
 $heading = "Create Note";
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    dd($_POST);
-} 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $errors = []; //$errors array to store error messages
+
+    if (strlen($_POST['body']) == 0) {
+        $errors['body'] = 'A body is required'; //$errors['body'] is adding a new key-value pair to the $errors array.
+    } //same as $errors = ['body' => 'A body is required']
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'The body cannot be more than 1000 characters';
+    }
+
+    if (empty($errors)) { //if no errors are found
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]); 
+    }
+}
 
 
 require "views/note-create.view.php";
