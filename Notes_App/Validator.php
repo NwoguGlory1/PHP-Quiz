0be70a4a75f79607570d0f358ad4d46b85925364
NwoguGlory1 <glorynwogu18@gmail.com>
2024-11-10 18:34:35 +0100

    Validation class now responds to an empty submitted body and a boody of more than 1000 characters using a static function

--- Notes_App/Validator.php
+++ Notes_App/Validator.php
@@ -2,8 +2,10 @@
 
 class Validator
 {
-    public function string($value)
+    public static function string($value, $min = 1, $max = INF)
     {
-        return strlen(trim($value))=== 0;
+        $value = trim($value);
+
+        return strlen($value) >= $min && strlen($value) < $max;
     }
 }
--- Notes_App/controllers/note-create.php
+++ Notes_App/controllers/note-create.php
@@ -10,15 +10,11 @@ $heading = "Create Note";
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $errors = []; //$errors array to store error messages
 
-    $validator = new Validator();
 
-    if ($validator ->string($_POST['body'])) {
-        $errors['body'] = 'A body is required'; //$errors['body'] is adding a new key-value pair to the $errors array.
+    if (!Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of not more than 1000 characters is required'; //$errors['body'] is adding a new key-value pair to the $errors array.
     } //same as $errors = ['body' => 'A body is required']
 
-    if (strlen($_POST['body']) > 1000) {
-        $errors['body'] = 'The body cannot be more than 1000 characters';
-    }
 
     if (empty($errors)) { //if no errors are found
         $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
